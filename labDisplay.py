e41ef5950a8148bb8e43f28e7f6a5b43acdae15c
Shriramana Sharma <samjnaa@gmail.com>
2019-06-06 19:01:29 +0530

    relicense to BSD-2-clause and add app icon

--- labDisplay.py
+++ labDisplay.py
@@ -1,21 +1,12 @@
 # LabDisplay widget
 # =================
+#
 # visualize CIE LAB/LCH colorspaces
 #
 # Copyright (C) 2015, Shriramana Sharma, samjnaa-at-gmail-dot-com
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Use, modification and distribution are permitted subject to the
+# "BSD-2-Clause"-type license stated in the accompanying file LICENSE.txt
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
--- rgb2lab-gui.py
+++ rgb2lab-gui.py
@@ -2,24 +2,15 @@
 
 # RGB2LAB GUI
 # ===========
+#
 # - convert color values from RGB to/from CIE LAB/LCH
 #   for sRGB gamut, D65 illuminant, 2° observer
 # - visualize CIE LAB and CIE LCH colorspaces
 #
 # Copyright (C) 2015, Shriramana Sharma, samjnaa-at-gmail-dot-com
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Use, modification and distribution are permitted subject to the
+# "BSD-2-Clause"-type license stated in the accompanying file LICENSE.txt
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
@@ -197,6 +188,7 @@ class MainWindow(QWidget):
         self.colorDisplay.setColor(QColor(Qt.transparent) if self.rgbHexInput.text() == "" else QColor.fromRgb(*self.readSpins("RGB")))
 
 app = QApplication([])
+app.setWindowIcon(QIcon("rgb2lab.png"))
 mainWindow = MainWindow()
 mainWindow.show()
 app.exec_()
